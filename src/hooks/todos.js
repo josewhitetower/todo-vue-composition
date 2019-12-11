332d1cf610942163705ac18abea28da278723505
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2019-12-11 16:25:47 +0100

    added persistency in localstorage

--- src/hooks/todos.js
+++ src/hooks/todos.js
@@ -1,25 +1,34 @@
 import { reactive } from "@vue/composition-api";
 const useTodo = () => {
     let state = reactive({
-        todos: [],
+        todos: JSON.parse(localStorage.getItem("todos") || "[]"),
         todo: {}
     })
 
     const addNewTodo = (title) => {
         state.todos.push({title, isDone:false});
-        state.todo = {}
+        state.todo = {};
+        save();
     }
 
     const removeTodo = (index) => {
         state.todos.splice(index);
+        save();
     }
 
     const toggleTodo = (index) => {
-        state.todos[index].isDone = !state.todos[index].isDone
+        state.todos[index].isDone = !state.todos[index].isDone;
+        save();
     }
+
     const editTodo = (index, title) => {
-        state.todos[index].title = title
+        state.todos[index].title = title;
+        save();
     }
+
+    const save = () => {
+        localStorage.setItem("todos", JSON.stringify(state.todos));
+    };
 
     return {
         state, addNewTodo, removeTodo, toggleTodo, editTodo
