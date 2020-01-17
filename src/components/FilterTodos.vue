3219548b94cc0f0407cc557fa6134ff0c77af5cb
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-01-17 13:57:26 +0100

    added filter

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex justify-end mt-4 pr-4 items-baseline">
+        <input type="radio" id="all" value="all" v-model="picked" class="mr-2">
+        <label for="all">All</label>
+        <input type="radio" id="done" value="done" v-model="picked" class="ml-8 mr-2">
+        <label for="done">Done</label>
+        <input type="radio" id="undone" value="undone" v-model="picked" class="ml-8 mr-2">
+        <label for="two">Undone</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            picked: "all"
+        }),
+
+        watch: {
+            picked() {
+                this.$emit('picked', this.picked)
+            }
+        }
+    }
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,12 +3,10 @@
         class="TodoList py-8 bg-red-500 text-center text-white font-sans rounded mt-8 shadow-2xl"
     >
         <h1 class="text-3xl uppercase font-thin mb-4">Todo List!</h1>
-        <!-- <ul>
-            <li v-for="(todo, index) in todos" :key="todo.id"><Todo :todo="todo" @remove="() => removeTodo(index)" @toggle="() => toggleTodo(index)" @edit="(value) => editTodo(index, value)"/></li>
-        </ul> -->
-        <draggable v-model="todos" group="people" @start="drag=true" @end="drag=false" :list="todos" @update="() => save(todos)">
-            <Todo v-for="(todo, index) in todos" :key="todo.id" :todo="todo" @remove="() => removeTodo(index)" @toggle="() => toggleTodo(index)" @edit="(value) => editTodo(index, value)"/>
+        <draggable @start="drag=true" @end="drag=false" :list="todos" @update="() => save(todos)">
+            <Todo v-for="(todo, index) in shownTodos" :key="todo.id" :todo="todo" @remove="() => removeTodo(index)" @toggle="() => toggleTodo(index)" @edit="(value) => editTodo(index, value)"/>
         </draggable>
+        <FilterTodos @picked="onPicked"/>
         <NewTodoForm @add="addNewTodo" />
     </div>
 </template>
@@ -18,18 +16,38 @@ import draggable from 'vuedraggable'
 import useTodo from '../hooks/todos'
 import Todo from './Todo'
 import NewTodoForm from './NewTodoForm'
+import FilterTodos from './FilterTodos'
 export default {
     components: {
         Todo,
         NewTodoForm,
+        FilterTodos,
         draggable,
     },
 
     setup() {
         const {state, addNewTodo, removeTodo, toggleTodo, editTodo, save} = useTodo();
-        const {todos} = state
+        const {todos} = state;
+        let filter = 'all';
         return {
-            todos, addNewTodo, removeTodo, toggleTodo, editTodo, save
+            todos, addNewTodo, removeTodo, toggleTodo, editTodo, save, filter
+        }
+    },
+
+    computed: {
+        shownTodos() {
+            if (this.filter === 'done') {
+                return this.todos.filter(todo => todo.isDone);
+            } else if (this.filter === 'undone') {
+                return this.todos.filter(todo => !todo.isDone);
+            }
+            return this.todos;
+        },
+    },
+
+    methods: {
+        onPicked(value) {
+            this.filter = value
         }
     }
 
